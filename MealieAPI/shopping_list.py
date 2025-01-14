1a86c8cf4410e96adbcf99bdba80e64a4de6da04
Raffaello Ippolito <86683344+redital@users.noreply.github.com>
2025-01-14 16:37:01 +0100

    fix

--- MealieAPI/shopping_list.py
+++ MealieAPI/shopping_list.py
@@ -63,107 +63,27 @@ def search_shopping_list(query, api_url=mealie_url, token=api_token):
         )
 
 
-def delete_shopping_list(slug, api_url=mealie_url, token=api_token):
+def delete_shopping_list(item_id, api_url=mealie_url, token=api_token):
     headers = get_headers(token)
     response = requests.delete(
-        "{}{}/{}".format(api_url, base_url, slug), headers=headers
+        "{}{}/{}".format(api_url, base_url, item_id), headers=headers
     )
 
     if response.status_code == 200:
-        print("\nEliminato {} - Status Code: {}".format(slug, response.status_code))
+        print("\nEliminato {} - Status Code: {}".format(item_id, response.status_code))
     else:
         print(
             "\nErrore in fase di eliminazione {} - Status Code: {} - Response: {}".format(
-                slug, response.status_code, response.text
+                item_id, response.status_code, response.text
             )
         )
-
-
-def create_shopping_list(recipe_name, api_url=mealie_url, token=api_token):
-    headers = get_headers(token)
-    data = {"name": recipe_name}
-
-    response = requests.post(
-        "{}{}".format(api_url, base_url),
-        data=json.dumps(data, ensure_ascii=False),
-        headers=headers,
-    )
-
-    if response.status_code == 201:
-        print(
-            "\nLista creata! - Status Code: {}, Response: {}".format(
-                response.status_code, response.json()
-            )
-        )
-        return response.json()
-    else:
-        print(
-            "\nErrore in fase di creazione! - Status Code: {}, Response: {}".format(
-                response.status_code, response.text
-            )
-        )
-
-
-def populate_shopping_list(recipe, slug, api_url=mealie_url, token=api_token, delete_if_failed=False):
-    headers = get_headers(token)
-
-    response = requests.put(
-        "{}{}/{}".format(api_url, base_url, slug),
-        data=recipe.serialize(),
-        headers=headers,
-    )
-
-    if response.status_code == 200:
-        print("\nLista popolata! - Status Code: {}".format(response.status_code))
-    else:
-        print(
-            "\nErrore in fase di collegamento! - Status Code: {}, Response: {}".format(
-                response.status_code, response.text
-            )
-        )
-        if delete_if_failed:
-            delete_shopping_list(slug, api_url, api_token)
-
-
-def create_shopping_list_from_url(url, include_tags=False, api_url=mealie_url, token=api_token):
-    data = {"url": url, "include_tags": include_tags}
-    headers = get_headers(token)
-
-    response = requests.post(
-        "{}{}/create-url".format(api_url, base_url),
-        data=json.dumps(data),
-        headers=headers,
-    )
-
-    if response.status_code == 201:
-        print(
-            "\nLista creata - URL: {}, Include Tags: {}, Status Code: {}, Response: {}".format(
-                url, include_tags, response.status_code, response.text
-            )
-        )
-    else:
-        print(
-            "\nParse Error - URL: {}, Include Tags: {}, Status Code: {}, Response: {}".format(
-                url, include_tags, response.status_code, response.text
-            )
-        )
-        return
-
-    slug = response.json()
-
-    if slug[-1].isdigit():
-        print(
-            "\nAttenzione questa Lista sembrerebbe essere un duplicato! Ti consiglio di andare a controllare"
-        )
-
-    return slug
 
 
 def delete_all_shopping_lists(api_url=mealie_url, token=api_token):
     recipe_list = get_all_shopping_lists(api_url=api_url, token=token)
 
     for i in recipe_list:
-        delete_shopping_list(i.slug)
+        delete_shopping_list(i.id)
 
 
 def empty_list(item_id, api_url=mealie_url, token=api_token):
