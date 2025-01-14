b408b59638175c0ca0c932efacbb4829beb97ed8
Raffaello Ippolito <86683344+redital@users.noreply.github.com>
2025-01-14 16:38:17 +0100

    add shopping list to delete all

--- scripts/delete_all.py
+++ scripts/delete_all.py
@@ -1,7 +1,8 @@
-from MealieAPI import recipe,tool,tag,food,unit
+from MealieAPI import recipe,tool,tag,food,unit,shopping_list
 
 unit.delete_all_units()
 food.delete_all_foods()
 tag.delete_all_tags()
 tool.delete_all_tools()
 recipe.delete_all_recipes()
+shopping_list.delete_all_shopping_lists()
